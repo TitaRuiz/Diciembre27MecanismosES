a054ec6e43af1c86dbc7e0a8ffe8a8758c5c3e60
TitaRuiz <ggp2002es@gmail.com>
2023-12-27 16:16:26 +0100

    16:16 Terminando el ejercicio 2

--- src/com/corenetworks/presentacion/Ejercicio1Clase.java
+++ src/com/corenetworks/presentacion/Ejercicio1Clase.java
@@ -2,18 +2,27 @@ package com.corenetworks.presentacion;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class ProbarFileWriter {
+public class Ejercicio1Clase {
     public static void main(String[] args) {
         //1. Declarar variables
         FileWriter fSalida = null;
+        Scanner teclado = new Scanner(System.in);
+        String frase = null;
         try {
-            fSalida = new FileWriter("primer_ejemplo.txt");
+            fSalida = new FileWriter("segundo_ejemplo.txt");
             //2. Acciones e/S
-            fSalida.write("Hola mundo desde java !!");
+            for(int i = 1; i <=5; i++){
+                System.out.println("Escriba la frase " + i);
+                frase = teclado.nextLine();
+                fSalida.write(frase+ "\n");
+
+            }
             //3. Cerrar los flujos
             fSalida.flush();
             fSalida.close();
+            teclado.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/com/corenetworks/presentacion/ProbarFileWriter.java
+++ src/com/corenetworks/presentacion/ProbarFileWriter.java
@@ -1,2 +1,22 @@
-package com.corenetworks.presentacion;public class ProbarFileWriter {
+package com.corenetworks.presentacion;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ProbarFileWriter {
+    public static void main(String[] args) {
+        //1. Declarar variables
+        FileWriter fSalida = null;
+        try {
+            fSalida = new FileWriter("primer_ejemplo.txt");
+            //2. Acciones e/S
+            fSalida.write("Hola mundo desde java !!");
+            //3. Cerrar los flujos
+            fSalida.flush();
+            fSalida.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
